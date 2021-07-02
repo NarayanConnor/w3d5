719bf0056e107308249203d62a79552510765ef0
Jonathan Wong <jonathan.wong8@baruchmail.cuny.edu>
2021-07-02 16:10:21 -0400

    commit 3

--- knight_travalis.rb
+++ knight_travalis.rb
@@ -14,21 +14,28 @@ class KnightPathfinder
 
     end
 
-    def add_arr(arr_1,arr_2)
+    def self.add_arr(arr_1,arr_2)
         row=arr_1[0]+arr_2[0]
         column=arr_1[1]+arr_2[1]
         [row,column]
     end
-    def self.valid_moves(pos_new)
-        right_up=
-        right_down
-        left_down=
-        
 
+    def self.valid_moves(pos)
+        valid_moves = []
+        possible = [ [-1,-2], [-1,2], [1,-2], [1,2], [-2,-1], [-2,1], [2,-1], [2,1] ]
+        possible.each do |move|
+            p = KnightPathfinder.add_arr(pos, move)
+            if (p[0]<8 && p[0] >= 0) && (p[1]<8 && p[1] >= 0)
+                valid_moves << p 
+            end
+        end
+        valid_moves
     end
 
-    def new_move_pos
-
+    def new_move_pos(pos)
+        valid = KnightPathfinder.valid_moves(pos)
+        valid.delete(pos)
+        @considered_pos = @considered_pos + valid
     end
 
 
