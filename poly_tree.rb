12c9d827399a34d134999dc539599f97910581f8
Jonathan Wong <jonathan.wong8@baruchmail.cuny.edu>
2021-07-02 14:22:24 -0400

    commit

--- poly_tree.rb
+++ poly_tree.rb
@@ -29,8 +29,29 @@ class PolyTreeNode
         new_parent.children<<self if new_parent != nil
     end
 
+    def inspect
+        h = Hash.new 
+        h[:value] = value
+        h[:children] = children.map { |child| child.value }
+        h[:parent] = parent
+        h.inspect
+    end
 
+    def add_child(child_node)
+        children << child_node
+    end
 
+    def remove_child(child_node)
+        children.delete(child_node)
+    end
+
+    def dfs(target_value)
+        return self if self.value == target_value
+        self.children.each do |child|
+            child.dfs(target_value)
+        end
+        return nil if self.children.empty?
+    end
      
 end
 
